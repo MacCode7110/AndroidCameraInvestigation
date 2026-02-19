0922d45c224c0852a26f7a95a3a123ad2f8072c0
MacCode7110 <mattmcal88@gmail.com>
2026-02-19 03:06:01 -0500

    Complete camera setup

--- app/src/main/java/com/example/androidcamerainvestigation/MainActivity.kt
+++ app/src/main/java/com/example/androidcamerainvestigation/MainActivity.kt
@@ -1,47 +1,201 @@
 package com.example.androidcamerainvestigation
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.Matrix
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.camera.core.ImageCapture
+import androidx.camera.core.ImageCaptureException
+import androidx.camera.core.ImageProxy
+import androidx.camera.view.CameraController
+import androidx.camera.view.LifecycleCameraController
+import androidx.camera.view.PreviewView
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.lifecycle.compose.LocalLifecycleOwner
 import com.example.androidcamerainvestigation.ui.theme.AndroidCameraInvestigationTheme
+import java.util.concurrent.Executor
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        if(!hasRequiredPermissions()) {
+            ActivityCompat.requestPermissions(
+                this,
+                CAMERA_PERMISSIONS,
+                0
+            )
+        }
         enableEdgeToEdge()
         setContent {
             AndroidCameraInvestigationTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                val controller = remember {
+                    LifecycleCameraController(applicationContext).apply {
+                        setEnabledUseCases (
+                            CameraController.IMAGE_CAPTURE
+                        )
+                    }
+                }
+
+                var capturedImage = remember {
+                    mutableStateOf<Bitmap?>(null)
+                }
+
+                Column(
+                        modifier = Modifier.padding(PaddingValues()),
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        verticalArrangement = Arrangement.SpaceEvenly
+                ) {
+                    Box(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .height(500.dp),
+                        contentAlignment = Alignment.Center
+                    ) {
+                        if (capturedImage.value != null) {
+                            Image (
+                                bitmap = capturedImage.value!!.asImageBitmap(),
+                                contentDescription = "Captured Photo",
+                                modifier = Modifier
+                                    .fillMaxSize()
+                                    .clip(RoundedCornerShape(2.dp)),
+                                contentScale = ContentScale.Crop
+                            )
+                        } else {
+                            CameraPreview(
+                                controller = controller,
+                                modifier = Modifier
+                                    .fillMaxSize()
+                                    .height(500.dp)
+                            )
+                        }
+                    }
+                    Spacer(modifier = Modifier.height(10.dp))
+                    Button(
+                        modifier = Modifier.fillMaxWidth(),
+                        onClick = {
+                            takePhoto(
+                                controller = controller,
+                                onPhotoTaken = {
+                                    bitmap -> capturedImage.value = bitmap
+                                }
+                            )
+                        },
+                        shape = RoundedCornerShape(1.dp),
+                        colors = ButtonDefaults.buttonColors(
+                            containerColor = Color.Blue,
+                            contentColor = Color.White
+                        )
+                    ) {
+                        Text("Take a picture", fontSize = 16.sp)
+
+                        //ML-Kit can be applied here
+                    }
                 }
             }
         }
     }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+    private fun takePhoto(
+        controller: LifecycleCameraController,
+        onPhotoTaken: (Bitmap) -> Unit
+    ) {
+        controller.takePicture(
+            ContextCompat.getMainExecutor(applicationContext),
+            object : ImageCapture.OnImageCapturedCallback() {
+                override fun onCaptureSuccess(image: ImageProxy) {
+                    super.onCaptureSuccess(image)
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    AndroidCameraInvestigationTheme {
-        Greeting("Android")
+                    val rotationDegrees = image.imageInfo.rotationDegrees
+
+                    val originalBitmap = image.toBitmap()
+
+                    val matrix = Matrix().apply {
+                        postRotate(rotationDegrees.toFloat())
+                    }
+
+                    val rotatedBitmap = Bitmap.createBitmap(
+                        originalBitmap,
+                        0,
+                        0,
+                        originalBitmap.width,
+                        originalBitmap.height,
+                        matrix,
+                        true
+                    )
+
+                    image.close()
+                    onPhotoTaken(rotatedBitmap)
+                }
+
+                override fun onError(exception: ImageCaptureException) {
+                    super.onError(exception)
+                    Log.e("Camera", "couldn't take photo", exception)
+                }
+
+            }
+        )
     }
-}
+
+    private fun hasRequiredPermissions(): Boolean {
+        return CAMERA_PERMISSIONS.all {
+            ContextCompat.checkSelfPermission(
+                applicationContext,
+                it
+            ) == PackageManager.PERMISSION_GRANTED
+        }
+    }
+
+    companion object {
+        private val CAMERA_PERMISSIONS = arrayOf(
+            Manifest.permission.CAMERA
+        )
+    }
+
+    @Composable
+    fun CameraPreview(
+        controller: LifecycleCameraController,
+        modifier: Modifier) {
+        val lifecycleOwner = LocalLifecycleOwner.current
+        AndroidView(
+            factory = {
+                PreviewView(it).apply {
+                    this.controller = controller
+                    controller.bindToLifecycle(lifecycleOwner)
+                }
+            },
+            modifier = modifier
+        )
+        }
+    }
